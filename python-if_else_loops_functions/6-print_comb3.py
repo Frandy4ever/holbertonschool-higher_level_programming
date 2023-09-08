4b0c53c1b8638b83b526be61860a2c64e225b81e
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 13:51:35 -0500

    update

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # Loop through the first digit from 0 to 9.
-for digit1 in range(10):
-    # Nested loop through the second digit from 0 to 9.
-    for digit2 in range(10):
-        # Check if the two digits are different 
-        # and the first digit is less than the second.
-        if digit1 != digit2 and digit1 < digit2:
-            # Print the smallest combination of two different digits.
-            print("{:02}, ".format(digit1 * 10 + digit2), end="")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
+
