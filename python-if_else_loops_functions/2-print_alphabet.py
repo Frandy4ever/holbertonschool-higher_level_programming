978f2752c6104a09902ca6c952193666bf62b8e3
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 12:48:10 -0500

    update 2-print file

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 # Loop through the ASCII values of lowercase letters ('a' to 'z').
 for letter in range(97, 123):
     # Print the lowercase letter, formatting it as a string without a newline.
-    print("{}".format(chr(letter)), end="")
+    print("{:c}".format(letter), end="")
