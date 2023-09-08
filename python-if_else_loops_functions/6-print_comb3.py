4467dd62e7d7c30a146180b26d342972879d4d09
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 10:22:44 -0500

    update 6-print file

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 # Loop through the first digit from 0 to 9.
-for digit1 in range(0, 10):
-    # Nested loop through the second digit from (digit1 + 1) to 9.
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            # If the current pair is 89, print it without a comma.
-            print("{}{}".format(digit1, digit2))
-        else:
-            # For all other pairs, print with a comma and a space.
-            print("{}{}".format(digit1, digit2), end=", ")
+for digit1 in range(10):
+    # Nested loop through the second digit from 0 to 9.
+    for digit2 in range(10):
+        # Check if the two digits are different and the first digit is less than the second.
+        if digit1 != digit2 and digit1 < digit2:
+            # Print the smallest combination of two different digits.
+            print("{:02}, ".format(digit1 * 10 + digit2), end="")
