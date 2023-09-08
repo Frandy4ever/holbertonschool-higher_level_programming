c94f2fe706c69f4776e5bcf5822aae0ccfefce42
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 10:50:25 -0500

    update 11-pow file

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def pow(a, b):
-    # Initialize the result to 1.
-    result = 1
-    
-    # Multiply 'a' by itself 'b' times to compute the power.
-    for _ in range(b):
-        result *= a
-    
-    # Return the computed result.
-    return result
+    # Return the result of raising 'a' to the power of 'b'.
+    return (a ** b)
