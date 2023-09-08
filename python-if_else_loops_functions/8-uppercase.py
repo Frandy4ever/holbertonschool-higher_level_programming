ba3764b5428f8791ac51edfc22eb9edd6f48236d
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 10:38:27 -0500

    add 8-uppercase file

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """Convert lowercase characters to uppercase in a string."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
