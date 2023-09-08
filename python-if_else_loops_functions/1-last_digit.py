1e3f28c20611cc04169d6106204d9e28d9aa6954
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 09:29:22 -0500

    add notes to 1-last file

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+# Import the 'random' module to generate random numbers.
 import random
+
+# Generate a random integer between -10000 and 10000 (inclusive).
 number = random.randint(-10000, 10000)
+
+# Calculate the last digit of the absolute value of 'number'.
 digit = abs(number) % 10
+
+# If 'number' is negative, make 'digit' negative as well.
 if number < 0:
     digit = -digit
+
+# Print the original number and its last digit.
 print("Last digit of {} is {} and is ".format(number, digit), end="")
+
+# Check the value of 'digit' and print an appropriate message.
 if digit > 5:
     print("greater than 5")
 elif digit == 0:
