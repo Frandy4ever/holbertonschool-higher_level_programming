d1dc055c9d42d735226d59069699db86909e3677
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 10:56:49 -0500

    add 12-fizzbuzz file

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    # Generate the FizzBuzz values for numbers from 1 to 100.
+    fizzbuzz_values = []
+    for number in range(1, 101):
+        value = ""
+        if number % 3 == 0:
+            value += "Fizz"
+        if number % 5 == 0:
+            value += "Buzz"
+        fizzbuzz_values.append(value or str(number))
+
+    # Print the FizzBuzz values separated by spaces.
+    print(" ".join(fizzbuzz_values))
+
+# Call the fizzbuzz function to generate and print the FizzBuzz sequence.
+fizzbuzz()
