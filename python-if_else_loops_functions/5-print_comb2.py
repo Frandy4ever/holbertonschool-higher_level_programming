18d7cf4272ffa81b3a79cc9cf039016618530b98
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 10:04:23 -0500

    update 5-print file

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # Loop through numbers from 0 to 99.
-for number in range(0, 100):
+for number in range(100):
     if number == 99:
-        # If the current number is 99, print it without a comma.
-        print("{}".format(number))
+        # If the current number is 99, print it with two digits and a newline.
+        print("{:02}\n".format(number), end="")
     else:
-        # For all other numbers, print with leading zeros and a comma.
-        print("{:02}".format(number), end=", ")
+        # For all other numbers, print with two digits, a comma, and a space.
+        print("{:02}, ".format(number), end="")
