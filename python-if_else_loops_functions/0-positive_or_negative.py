8b4f4004fb153470773f9bf8a336aa5da18d3896
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 09:31:26 -0500

    add note to 0-positive file

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+# Import the 'random' module to generate random numbers.
 import random
+
+# Generate a random integer between -10 and 10 (inclusive).
 number = random.randint(-10, 10)
+
+# Check the value of 'number' and print an appropriate message.
 if number > 0:
+    # If 'number' is greater than 0, it's considered positive.
     print("{} is positive".format(number))
 elif number == 0:
+    # If 'number' is equal to 0, it's considered zero.
     print("{} is zero".format(number))
 else:
+    # If 'number' is less than 0, it's considered negative.
     print("{} is negative".format(number))
