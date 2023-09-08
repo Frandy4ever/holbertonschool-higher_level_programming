65c3a74668ac4a70910ece759055c225c0a02a5a
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 16:47:04 -0500

    add 0-add file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
-    """Create a copy of the string without the character at position n."""
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add  # Import the 'add' function from the 'add_0' module.
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))  # Calculate and print the sum of 1 and 2 using the 'add' function.
+
