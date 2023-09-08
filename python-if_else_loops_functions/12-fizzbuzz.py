46db7e10865c037ab0c88bda2204dff6a2f9dd1c
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 11:02:00 -0500

    update 12-fizzbuzz file

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    # Generate the FizzBuzz values for numbers from 1 to 100.
-    fizzbuzz_values = []
+    # Loop through numbers from 1 to 100.
     for number in range(1, 101):
-        value = ""
-        if number % 3 == 0:
-            value += "Fizz"
-        if number % 5 == 0:
-            value += "Buzz"
-        fizzbuzz_values.append(value or str(number))
-
-    # Print the FizzBuzz values separated by spaces.
-    print(" ".join(fizzbuzz_values))
+        if number % 3 == 0 and number % 5 == 0:
+            # If the number is divisible by both 3 and 5, print "FizzBuzz" without a space.
+            print("FizzBuzz", end=" ")
+        elif number % 3 == 0:
+            # If the number is divisible by 3, print "Fizz" without a space.
+            print("Fizz", end=" ")
+        elif number % 5 == 0:
+            # If the number is divisible by 5, print "Buzz" without a space.
+            print("Buzz", end=" ")
+        else:
+            # For all other numbers, print the number itself without a space.
+            print(number, end=" ")
