3944147e74881a2d62579c96907f8cac1d172f2f
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 09:51:10 -0500

    revert 3-print back

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 # Loop through the ASCII values of lowercase letters ('a' to 'z').
-'''
 for letter in range(ord('a'), ord('z') + 1):
     # Check if the current letter is not 'q' and not 'e'.
     if chr(letter) not in ['q', 'e']:
         # Print the lowercase letter without a newline.
-        print("{}".format(chr(letter)), end="")'''
-        
-# Loop through the ASCII values of lowercase letters ('a' to 'z').
-for letter in range(97, 123):
-    # Check if the current letter is not 'q' and not 'e'.
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        # Print the lowercase letter, formatting it as a string without a newline.
         print("{}".format(chr(letter)), end="")
