78eb9729e437ceb1be02a6d7e1ae99881d026b5c
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 10:31:57 -0500

    update 7-islower file

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    # Check if the Unicode code point of the character is between the range of lowercase letters.
-    return ord('a') < ord(c) <= ord('z')
-  # Example usage:
-character = 'a'
-result = islower(character)
-print(result)  # Output will be True for 'a'
+    """Check for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
