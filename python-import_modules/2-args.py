b15d74a58b7144b33ee08c1e87eb266a5176d960
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 19:11:30 -0500

    update

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys  # Import the 'sys' module, which provides access to command-line arguments.
+    # Print the number of and list of arguments.
+    import sys
 
-    count = len(sys.argv) - 1  # Calculate the number of command-line arguments provided, excluding the script name.
-
+    count = len(sys.argv) - 1
     if count == 0:
-        print("0 arguments.")  # If no arguments are provided (excluding the script name), print "0 arguments."
+        print("0 arguments.")
     elif count == 1:
-        print("1 argument:")  # If only one argument is provided (excluding the script name), print "1 argument:"
+        print("1 argument:")
     else:
-        print("{} arguments:".format(count))  # If more than one argument is provided, print the count followed by "arguments:"
-
+        print("{} arguments:".format(count))
     for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))  # Print each argument along with its position (index + 1).
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
