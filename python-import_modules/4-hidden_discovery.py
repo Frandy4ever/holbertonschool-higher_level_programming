a869929cc72e53ee0ca5a9927ac2d2f5906cae67
Frandy Slueue <codebreeder@outlook.com>
2023-09-08 19:17:56 -0500

    update

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # Print all non-private names defined by hidden_4 module.
+    """Print all names defined by hidden_4 module."""
     import hidden_4
 
-    # Filter out names that start with double underscores and print the rest.
-    for name in dir(hidden_4):
-        if not name.startswith("__"):
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
             print(name)
