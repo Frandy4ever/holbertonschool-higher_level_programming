6f520c8cccc40b9997e5c2d9e157c0ad57676bdf
Frandy Slueue <codebreeder@outlook.com>
2023-09-12 17:28:14 -0500

    update 11-delete file

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     # Check if the index is out of range or negative
-    if idx < 0 or idx >= len(my_list):
-        return my_list  # Return the original list unchanged
-
-    # Create a new list with elements before the index and after the index
-    new_list = my_list[:idx] + my_list[idx+1:]
+    if idx >= 0 and idx < len(my_list):
+       del my_list[idx]  # Return the original list unchanged
 
     return new_list
