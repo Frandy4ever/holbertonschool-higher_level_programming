ff616fb9264a6ca88cced467878d26d322568487
Frandy Slueue <codebreeder@outlook.com>
2023-09-12 18:24:13 -0500

    update 6-print file

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
+    for i, row in enumerate(matrix):
+        for j, value in enumerate(row):
+            print(f"{value:d}", end="")
+            if j != (len(row) - 1):
+                print(" ", end="")
         print("")
