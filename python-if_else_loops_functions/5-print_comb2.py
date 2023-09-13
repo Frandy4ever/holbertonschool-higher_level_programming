1e63ab6f937ab5bd36a8eb1943226dd41258e373
Frandy Slueue <codebreeder@outlook.com>
2023-09-12 22:03:31 -0500

    update 5-print file

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# for num in range(100):
+#     print(f'{num:02d}', end=', ' if num < 99 else '\n')
 # Loop through numbers from 0 to 99.
 for number in range(0, 100):
     if number == 99:
