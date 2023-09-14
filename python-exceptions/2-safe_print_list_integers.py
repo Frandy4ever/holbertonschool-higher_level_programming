d8ec800ee629080083d975daf2eb25a16e19fd6d
Frandy Slueue <codebreeder@outlook.com>
2023-09-14 17:51:08 -0500

    update 2-safe file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+   
+    counted_track = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            counted_track += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (counted_track)
+  
