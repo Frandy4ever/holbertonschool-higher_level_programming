6b1800bf85b14bd3ddee8b410fc3eb11c971fe0b
Frandy Slueue <codebreeder@outlook.com>
2023-09-14 18:21:59 -0500

    update 2-safe file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
    
     counted_track = 0
-    for i in range(0, x):
+    for indx in range(0, x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[indx]), end="")
             counted_track += 1
         except (ValueError, TypeError):
             continue
     print("")
     return (counted_track)
-  
