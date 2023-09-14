5a155f2775e397d56ea3fabbd188763179c0c02b
Frandy Slueue <codebreeder@outlook.com>
2023-09-14 18:24:13 -0500

    update 4-list file

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,9 +2,9 @@
 def list_division(my_list_1, my_list_2, list_length):
   
     new_list = []
-    for i in range(0, list_length):
+    for indx in range(0, list_length):
         try:
-            quotient = my_list_1[i] / my_list_2[i]
+            quotient = my_list_1[indx] / my_list_2[indx]
         except TypeError:
             print("wrong type")
             quotient = 0
