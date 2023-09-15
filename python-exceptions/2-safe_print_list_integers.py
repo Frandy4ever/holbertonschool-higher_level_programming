252fabebfd92f9e58c1db92732d3f5cadcae303b
Frandy Slueue <codebreeder@outlook.com>
2023-09-14 22:26:28 -0500

    update 2-safe file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-   counted_track = 0
+    counted_track = 0
     for indx in range(0, x):
         try:
             print("{:d}".format(my_list[indx]), end="")
