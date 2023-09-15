d942e2a1f82c8776c439d41dbde4b43280862a58
Frandy Slueue <codebreeder@outlook.com>
2023-09-14 22:52:46 -0500

    update 4-square file

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
-"""Define a class called Square"""
+"""Define a class Square."""
 
 
 class Square:
-  
-    """Constructor method for initializing a Square object"""
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """Set the 'size' attribute to the provided 'size' parameter
-        (default to 0)"""
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
-    """Getter method for 'size' attribute, used as a property"""
     @property
     def size(self):
-        """Return the private attribute '__size'"""
+        """Get/set the current size of the square."""
         return (self.__size)
 
-    """Setter method for 'size' attribute, used as a property"""
     @size.setter
     def size(self, value):
-        """Check if the provided 'value' is not an 
-        integer, raise a TypeError"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        """Check if the provided 'value' is negative, raise a ValueError"""
         elif value < 0:
             raise ValueError("size must be >= 0")
-        """Set the private attribute '__size' to the provided 'value'"""
         self.__size = value
 
-    """Method to calculate the area of the square"""
     def area(self):
-        """Return the area, which is the square of the '__size' attribute"""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
