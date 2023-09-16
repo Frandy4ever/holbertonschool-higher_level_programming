a5d099367d2dbc3605a6fe55fa0bb01785dce3ab
Frandy Slueue <codebreeder@outlook.com>
2023-09-16 15:10:48 -0500

    update 3-rectangle file

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -25,7 +25,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -37,9 +37,13 @@ class Rectangle:
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return(self.__width * 2) + (self.__height * 2)
+        return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return (('#'*self.__width + "\n")*self.__height)[:-1]
+        return ('#' * self.__width + "\n") * self.__height
+
+    def __repr__(self):
+        return f'Rectangle({self.__width}, {self.__height})'
+
