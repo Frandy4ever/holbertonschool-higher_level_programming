607106466117ee2fc1ef0ec64bd12a17231a9334
Frandy Slueue <codebreeder@outlook.com>
2023-09-16 14:42:38 -0500

    general update to all files

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+Rectangle = __import__('8-rectangle').Rectangle
+
+new_rectangle_1 = Rectangle(8, 4)
+new_rectangle_2 = Rectangle(2, 3)
+
+if new_rectangle_1 is Rectangle.bigger_or_equal(new_rectangle_1, new_rectangle_2):
+    print("new_rectangle_1 is bigger or equal to new_rectangle_2")
+else:
+    print("new_rectangle_2 is bigger than new_rectangle_1")
+
+
+new_rectangle_2.width = 10
+new_rectangle_2.height = 5
+if new_rectangle_1 is Rectangle.bigger_or_equal(new_rectangle_1, new_rectangle_2):
+    print("new_rectangle_1 is bigger or equal to new_rectangle_2")
+else:
+    print("new_rectangle_2 is bigger than new_rectangle_1")
--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+new_square = Rectangle.square(5)
+print("Area: {} - Perimeter: {}".format(new_square.area(), new_square.perimeter()))
+print(new_square)
