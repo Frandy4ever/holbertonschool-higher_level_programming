c5e3f06ab604b1b4fc0f8e3df80d90eb43afadca
Frandy Slueue <codebreeder@outlook.com>
2023-09-16 14:53:14 -0500

    update 3-rectangle file

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,17 +1,45 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class."""
 
 
-Rectangle = __import__('3-rectangle').Rectangle
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
-new_gect = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(new_gect.area(), new_gect.perimeter()))
+    @property
+    def width(self):
+        return self.__width
 
-print(str(new_gect))
-print(repr(new_gect))
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-print("--")
+    @property
+    def height(self):
+        return self.__height
 
-new_gect.width = 10
-new_gect.height = 3
-print(new_gect)
-print(repr(new_gect))
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return(self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return (('#'*self.__width + "\n")*self.__height)[:-1]
