5a121913887b3ac0640cdf4bdf708b13938087a0
Frandy Slueue <codebreeder@outlook.com>
2023-09-16 14:31:05 -0500

    update 3-rectangle file

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+
+Rectangle = __import__('3-rectangle').Rectangle
+
+new_gect = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(new_gect.area(), new_gect.perimeter()))
+
+print(str(new_gect))
+print(repr(new_gect))
+
+print("--")
+
+new_gect.width = 10
+new_gect.height = 3
+print(new_gect)
+print(repr(new_gect))
