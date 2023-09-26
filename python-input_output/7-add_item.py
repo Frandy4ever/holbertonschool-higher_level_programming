ad3d4a7ba26348096661dff93c99a82fcc5559a2
Frandy Slueue <codebreeder@outlook.com>
2023-09-26 10:30:00 -0500

    update 7-add file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,37 +1,16 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""
+adds all arguments to a Python list, and then save them to a file
+"""
 import sys
-import json
-import os
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
 
 
-def save_to_json_file(my_obj, filename):
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def load_from_json_file(filename):
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-
-"""Define the file name for saving the list"""
-filename = "add_item.json"
-
-"""Check if the file exists and load its contents if it does"""
-if os.path.exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
-
-"""Add all command line arguments to the list"""
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
-"""Save the updated list as a JSON representation"""
-save_to_json_file(my_list, filename)
-
-"""Print the updated list"""
-print("List after adding arguments:", my_list)
+open("add_item.json", "a")
+try:
+    l = load_from_json_file("add_item.json")
+except ValueError:
+    l = []
+save_to_json_file(l + sys.argv[1:], "add_item.json")
