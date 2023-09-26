8f8c9c10354d9a29091805fed22b72ccc525181d
Frandy Slueue <codebreeder@outlook.com>
2023-09-26 09:50:19 -0500

    update 0-read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+"""Defines a function that reads a UTF-8 text file"""
+
+def read_file(filename=""):
+  with open(filename, encoding="utf-8") as file:
+    print(file.read(), end="")
