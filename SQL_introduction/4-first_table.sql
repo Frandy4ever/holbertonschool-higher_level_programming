0223dbd67e95a5ce5f19b5482f38011b0423aeaa
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-23 00:00:50 -0500

    update table creation code

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
 -- Create db table with some attributes
-CREATE TABLE first_table
-    id INT,
-    name VARCHAR(256);
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(256)
+);
