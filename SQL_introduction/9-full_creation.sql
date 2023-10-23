92cdec9f2bf7d723f147cee9836aa47191e85713
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-23 11:02:05 -0500

    Create additional db table multi-records

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,16 +1,6 @@
--- Create additional db table multi-records
-USE hbtn_0c_0;
-
--- Check if the table exists, and create it if it doesn't
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
-
--- Insert the specified rows
-INSERT INTO second_table (id, name, score) VALUES
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
+-- CREATE SECOND TABLE and insert into
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256),score int);
+INSERT INTO second_table (id, name, score) value (1, 'John', 10);
+INSERT INTO second_table (id, name, score) value (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) value (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) value (4, 'George', 8);
