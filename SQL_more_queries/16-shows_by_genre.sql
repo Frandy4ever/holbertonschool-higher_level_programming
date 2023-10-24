0df2b205cad383d24e30a7a442959d333e2bb3d3
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-23 21:51:44 -0500

    Listing data from various tables records on a db

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Listing data from various tables records on a db
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
