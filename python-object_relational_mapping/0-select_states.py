5b3169be4bf009d9ef2c95b0ec3142364c33ebcc
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-24 20:12:41 -0500

    update modify code in 0-select file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
+"""
+Module list state
+"""
 import sys
 import MySQLdb
 
+
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    query = "SELECT id,name FROM states ORDER by id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        print(r)
+    cur.close()
+    conn.close()
+
+
 if __name__ == "__main__":
-    # Check that the correct number of command-line arguments is provided.
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database_name>")
-        sys.exit(1)
-
-    # Extract the command-line arguments.
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Connect to the MySQL server running on localhost at port 3306.
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
-
-    # Create a cursor.
-    cursor = db.cursor()
-
-    # Execute the SQL query to fetch states with names starting with 'N'.
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
-
-    # Fetch and display the results.
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-
-    # Close the cursor and the database connection.
-    cursor.close()
-    db.close()
+    main()
