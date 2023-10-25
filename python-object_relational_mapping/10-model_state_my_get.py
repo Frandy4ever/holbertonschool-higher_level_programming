8de5535144ccbb108e681e41344899b88c83db96
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-24 21:54:20 -0500

    update 10-model file

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """
-prints the State object with the name passed as
-argument from the database hbtn_0e_6_usa"""
+Prints the State object with the name passed as argument from the db
+"""
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
-    engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost/{}'
-    .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-    session = Session(engine)
-    match = sys.argv[4]
-    re = session.query(State).filter_by(name=match).first()
-    if re is not None:
-        print(str(re.id))
-    else:
-        print("Not found")
-    session.close()
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    print("Not found" if not state else state.id)
