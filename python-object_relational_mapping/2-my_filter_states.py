5db0ae1c86b786db380b7f8a416960a764a32b42
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-24 21:21:19 -0500

    update 2-my_filter file

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-""" script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument."""
+"""
+Write a script that takes in an argument and
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
 import sys
 import MySQLdb
 
+
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    search = sys.argv[4]
+    query = """SELECT * FROM states where name = '{:s}'
+            ORDER by id ASC""".format(search)
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        if r[1] == search:
+            print(r)
+    cur.close()
+    conn.close()
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
-    cursor = db.cursor()
-
-    state_name = sys.argv[4]
-
-    query = "SELECT * FROM states WHERE name = '{}' " \
-            "ORDER BY id ASC".format(state_name)
-    cursor.execute(query)
-
-    result_states = cursor.fetchall()
-
-    for state in result_states:
-        print(state)
-
-    cursor.close()
-    db.close()
+    main()
