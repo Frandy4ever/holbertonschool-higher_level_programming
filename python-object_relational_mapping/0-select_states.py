9fc28d9eef2c79880e5d0fbf15f41fc7bc2531ca
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-24 20:11:11 -0500

    update 0-select file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # Check that the correct number of command-line arguments is provided.
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database_name>")
+        sys.exit(1)
+
+    # Extract the command-line arguments.
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Connect to the MySQL server running on localhost at port 3306.
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+
+    # Create a cursor.
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`")
-    [print(state) for state in cursor.fetchall()]
+
+    # Execute the SQL query to fetch states with names starting with 'N'.
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+    # Fetch and display the results.
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Close the cursor and the database connection.
+    cursor.close()
+    db.close()
