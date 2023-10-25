0cac8cb3144f54c821ea17630e939cab1ba5e969
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-24 21:26:38 -0500

    update 2-my_filter file

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
 """
-Write a script that takes in an argument and
-displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument.
+Lists all values in the states tables of a database where name
+matches the argument
 """
 import sys
 import MySQLdb
 
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-def main():
-    conn = MySQLdb.connect(
-                        host="localhost",
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        charset="utf8"
-                            )
-    cur = conn.cursor()
-    search = sys.argv[4]
-    query = """SELECT * FROM states where name = '{:s}'
-            ORDER by id ASC""".format(search)
-    cur.execute(query)
-    row = cur.fetchall()
-    for r in row:
-        if r[1] == search:
-            print(r)
-    cur.close()
-    conn.close()
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
-
-if __name__ == "__main__":
-    main()
+    for state in states:
+        print(state)
