b7af1fa351d70c7ea7c39de97b4017b3d7ca381b
Frandy Slueue <frandy4ever@gmail.com>
2023-11-22 19:20:01 -0600

    update

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 myArray.forEach(function (language) {
     console.log(language);
