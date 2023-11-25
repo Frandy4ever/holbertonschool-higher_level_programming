57e82536b28fb0c7cfbe4db81f3f9f70a6ea5321
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-25 12:31:15 -0600

    update 0-javascript to print content of const var

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-// Prints the value of a const variable.
-const myVar = 'Javascript is amazing';
+// Prints the content of a const variable.
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
