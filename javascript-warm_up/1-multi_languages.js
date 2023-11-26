e90b20dd72e55c78773a74f13f13818f38824940
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-25 18:14:47 -0600

    print the value of 3 vars

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// Prints the value of 3 string variables.
+const c = 'C is fun';
+const python = 'Python is cool';
+const js = 'Javascript is amazing';
+console.log(c);
+console.log(python);
+console.log(js);
