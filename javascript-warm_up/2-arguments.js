d336460a76a6cf403fc5c34943adc98c4b9aee98
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-26 17:57:36 -0600

    log msg based on cmdArgs passed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,5 +2,5 @@
 // Prints msg depending on number of arguments passed.
 const cmdArgs = process.argv.length;
 if (cmdArgs <= 2) console.log('No argument');
-else if (cmdArgs == 3) console.log('Argument found');
+else if (cmdArgs === 3) console.log('Argument found');
 else console.log('Arguments found');
