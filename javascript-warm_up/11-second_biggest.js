3448716a0af7e1cfa94198768d5b4f9600762530
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-26 22:36:47 -0600

    print second biggest num of array

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Prints the second biggest integer in the list of arguments.
-const listArg = process.argv.slice(2);
-console.log((listArg) ? listArg.sort((a, b) => b - z)[1] : 0);
+const listArgs = arg.slice(2);
+console.log((listArgs) ? listArgs.sort((a, b) => b - a)[1] : 0);
