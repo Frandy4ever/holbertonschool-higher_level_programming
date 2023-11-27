8bf0364b07b3e9f6680b8ce12414ddd4e76a038c
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-26 18:12:54 -0600

    print value of the first argm passed

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 // Prints value of the first argument passed.
-const cmdArgs = process.argv[2];
-
-console.log((cmdArgs) ? cmdArgs : 'No argument');
+console.log((process.argv[2]) ? process.argv[2] : 'No argument');
