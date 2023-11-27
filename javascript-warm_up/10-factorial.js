3099dff7332a6f1cef84cddc832493d834917d76
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-26 20:05:15 -0600

    Print the factorial where  of the first argument

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 // Prints the factorial where  of the first argument.
 const factorial = (n) => {
-  if (n === 0) {
-    return 1;
-  }
+  if (isNaN(n) || n <= 0) return 1;
   return n * factorial(n - 1);
 }
 console.log(factorial(parseInt(process.argv[2])));
