2d1e150e6e0dc364940a3439aae7566eb01c7ea7
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-26 19:52:21 -0600

    add first 2 argments

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Print the addition of first and second arguments using add(a, b) prototype.
 const add = (a, b) => a + b;
-console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])))
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
