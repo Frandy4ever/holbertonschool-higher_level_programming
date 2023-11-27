86d732dceefb4b89b97f5bc81fb89f4e6d8a641a
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-26 18:59:24 -0600

    print value of array of string

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 // Prints values of array of strings
 const randomLangs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-randomLangs.forEach(lang => console.log(lang));
+// randomLangs.forEach(lang => console.log(lang));
+for (const lang of randomLangs) console.log(lang);
