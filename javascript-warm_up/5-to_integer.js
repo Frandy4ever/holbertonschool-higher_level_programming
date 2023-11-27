534be809298182867b8a721d04f33ec5df80eb00
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-26 18:48:30 -0600

    print first arg if isInt

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,4 @@
 #!/usr/bin/node
+// Prints first argument if it can be converted to an integer
+const intArgs = parseInt(process.argv.slice(2));
+console.log(intArgs ? `My number: ${intArgs}` : 'Not a number');
