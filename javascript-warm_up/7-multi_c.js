fe0f0bd7f20d9e7878d8aec46fdf4ad0ec20b462
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-26 19:09:17 -0600

    print x time a given phrase

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Print x times “C is fun” while x is the first argument of the script
 const x = parseInt(process.argv[2]);
-if (x) for (let i = 0; i <= x; i++) console.log('C is fun');
+if (x) for (let i = 0; i < x; i++) console.log('C is fun');
