8c1b519ad6cb372bf03cff964d8a5f60e18b04cc
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-28 14:21:29 -0600

    Function that increments the integer value

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+// Function that increments the integer value
+const myObject = {
+  type: 'object',
+  value: 12
+  };
+
+  console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+}
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
