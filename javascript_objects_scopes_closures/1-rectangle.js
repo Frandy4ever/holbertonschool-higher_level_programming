9ba6a1e61541053e7d81911919cdc4b02a763af8
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-28 18:28:35 -0600

    create rectangle class

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// Rectangle class with constructor having two arguments w and h
+module.exports = class Rectangle {
+    constructor (w, h) {
+        this.width = w;
+        this.height = h;
+    };
+}
