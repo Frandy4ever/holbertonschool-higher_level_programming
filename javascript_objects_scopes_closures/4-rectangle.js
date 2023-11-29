866118e6b0a9a11c76ca8903a4166c1a689ff499
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-29 17:50:21 -0600

    Rectangle class defining a rectangle with print(), rotate(), and double() methods

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+// Rectangle class defining a rectangle with print(), rotate(), and double() methods
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w && h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
