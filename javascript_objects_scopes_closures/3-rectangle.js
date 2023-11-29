383bbf0a4c25200cf53fc0d9dac4aa3647771458
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-29 11:12:03 -0600

    Prints a rectangle with a print() instance method

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+// Prints a rectangle with a print() instance method
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w && h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
+
+print () {
+  if (this.width && this.height) {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
