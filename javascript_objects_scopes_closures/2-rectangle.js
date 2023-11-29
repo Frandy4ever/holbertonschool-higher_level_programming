de327b738309537dbf5ab37273553386785c3d44
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-29 10:58:49 -0600

    Rectangle class with constructor having two arguments w and h

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// Rectangle class with constructor having two arguments w and h
+module.exports = class Rectangle {  
+  constructor(w, h) {
+    this.width = (w  > 0) ? w : 0;
+    this.height = (h > 0) ? h : 0;
+  }
+};
