8b2c31e91f7c2020d1cf3bc977179ebddf9f91ed
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-28 18:23:08 -0600

    create empty rectangle object

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 // Empty class Rectangle that defines a rectangle.
-class Rectangle {
-  // An empty class object
-}
-
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
+module.exports.Rectangle = class Rectangle {};
