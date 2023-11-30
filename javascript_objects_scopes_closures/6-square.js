1dbd6ab26b4588c29c80a0adee47b475a13a6492
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-29 21:45:29 -0600

    Class square that defines a square

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// Class square that defines a square and inherits from square of 5-square.js
+const Square5 = require('./5-square');
+class Square extends Square5 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
