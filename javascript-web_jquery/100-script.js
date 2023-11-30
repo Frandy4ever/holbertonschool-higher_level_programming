8bbf4286af60f13a59cf750a55cb081b9c1b8cbf
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-30 09:42:02 -0600

    update text color imported from head

---
+++ javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Update text color of the HTML tag HEADER to red (#FF0000):
+// imported from the head tag
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  (header).style.color = '#FF0000';
+});
