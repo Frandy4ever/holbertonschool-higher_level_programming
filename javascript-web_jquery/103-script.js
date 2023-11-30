aa6efec159b47dfdd90e673cd0c0582dcbc72107
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-30 10:01:30 -0600

    Fetches ahd prints how to say “Hello” depending of the language

---
+++ javascript-web_jquery/103-script.js
@@ -0,0 +1,34 @@
+// Fetches ahd prints how to say “Hello” depending of the language
+$(document).ready(function () {
+  // Function to fetch and display the translation
+  function fetchTranslation () {
+    const languageCode = $('#language_code').val().trim();
+
+    if (languageCode === '') {
+      alert('Please enter a language code.');
+      return;
+    }
+
+    $.ajax({
+      url: `https://www.fourtonfish.com/hellosalut/hello/${languageCode}/`,
+      method: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        $('#hello').text(data.hello);
+      },
+      error: function () {
+        console.log('Error fetching translation.');
+      }
+    });
+  }
+
+  $('#btn_translate').click(function () {
+    fetchTranslation();
+  });
+
+  $('#language_code').keyup(function (event) {
+    if (event.keyCode === 13) {
+      fetchTranslation();
+    }
+  });
+});
