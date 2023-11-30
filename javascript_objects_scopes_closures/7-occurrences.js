29f59ff0ed7d220c4fa9d7f0350acabd78708927
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-29 21:55:43 -0600

    Returns the number of occurrences in a list

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 // Returns the number of occurrences in a list
 exports.nbOccurrences = function (list, searchElement) {
-  const occurrences = list.reduce((count, element) => {
-    return element === searchElement ? count + 1 : count;
-  }, 0);
-
-  return occurrences;
+  return list.filter(x => x === searchElement).length;
 };
