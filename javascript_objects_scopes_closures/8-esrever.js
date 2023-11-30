5c8bf557d85b13a490ca19feb766f139de4afd99
Frandy G Slueue <frandy4ever@gmail.com>
2023-11-29 22:04:14 -0600

    Clone the original list to avoid modifying it directly

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+// Clone the original list to avoid modifying it directly
+
+exports.esrever = function (list) {
+  const reversedList = [...list];
+
+  for (let i = 0, j = reversedList.length - 1; i < j; i++, j--) {
+    const temp = reversedList[i];
+    reversedList[i] = reversedList[j];
+    reversedList[j] = temp;
+  }
+
+  return reversedList;
+};
