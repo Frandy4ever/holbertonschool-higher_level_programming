62960e65a227cadc7e3338c0710776321fc795e7
Frandy G Slueue <frandy4ever@gmail.com>
2023-12-05 16:12:16 -0600

    Prints the number of movies where the character “Wedge Antilles” is present

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+// Prints the number of movies where the character “Wedge Antilles” is present
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+request(url, function (err, response, body) {
+  if (err) { console.error(err); }
+  const results = JSON.parse(body).results;
+  for (const film of results) {
+    for (const character of film.characters) {
+      if (character.endsWith('/18/')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
