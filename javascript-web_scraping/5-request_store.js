cf5637c55bd5126a5d9ccc330442bd7e211cbb29
Frandy G Slueue <frandy4ever@gmail.com>
2023-12-05 16:15:02 -0600

    Gets the contents of a webpage and stores it in a file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+// Gets the contents of a webpage and stores it in a file.
+const request = require('request');
+const fileSys = require('fs');
+const url = process.argv[2];
+const fileName = process.argv[3];
+request(url, function (err, response, body) {
+  if (err) { console.error(err); }
+  fileSys.writeFile(fileName, body, 'utf-8', function (err) {
+    if (err) { console.error(err); }
+  });
+});
