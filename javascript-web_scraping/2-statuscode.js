c9d6cde9678f1a0b866a56000f59ec173afbd694
Frandy G Slueue <frandy4ever@gmail.com>
2023-12-05 16:06:41 -0600

    Displays the status code of a GET request

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// Displays the status code of a GET request
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response) {
+  if (err) { console.error(err); }
+  console.log('code:', response.statusCode);
+});
