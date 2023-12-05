031c6a0e89a60f85dae2df2d2a265dc565d759da
Frandy G Slueue <frandy4ever@gmail.com>
2023-12-05 16:18:04 -0600

    Computes the number of tasks completed by user id

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,19 @@
 #!/usr/bin/node
-
-
-
-
+// Computes the number of tasks completed by user id.
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) { console.error(err); }
+  const results = JSON.parse(body);
+  const dict = {};
+  for (const task of results) {
+    if (task.completed === true) {
+      if (task.userId in dict) {
+        dict[task.userId]++;
+      } else {
+        dict[task.userId] = 1;
+      }
+    }
+  }
+  console.log(dict);
+});
