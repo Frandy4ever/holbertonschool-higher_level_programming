803311c19b1ba33f723ffd66fa2e7a46e3115e60
Frandy G Slueue <frandy4ever@gmail.com>
2023-12-05 12:59:12 -0600

    Reads and prints the content of a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Reads and prints the content of a file.
-const fs = require('fs');
+const fileSys = require('fs');
 const file = process.argv[2];
-fs.readFile(file, 'utf-8', function (err, data) {
+fileSys.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     console.error(err);
   } else {
